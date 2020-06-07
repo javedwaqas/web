7860f1c69cd2fd0f2aecfc4a44b67622da002579
Waqas Javed <waqasjaved@gmail.com>
2020-06-07 10:03:51 +0400

    menu close icon added

--- main.js
+++ main.js
@@ -1,24 +1,41 @@
 const menuIcon = document.getElementById("menu-icon");
+const menuCloseIcon = document.getElementById("menu-close-icon");
 const slideoutMenu = document.getElementById("slideout-menu");
 const searchIcon = document.getElementById("search-icon");
 const searchBox = document.getElementById("searchbox");
 
-searchIcon.addEventListener('click', function () {
-  if (searchBox.style.top == '72px') {
-    searchBox.style.top = '24px';
-    searchBox.style.pointerEvents = 'none';
-  } else {
-    searchBox.style.top = '72px';
-    searchBox.style.pointerEvents = 'auto';
-  }
-});
+// searchIcon.addEventListener('click', function () {
+//   if (searchBox.style.top == '72px') {
+//     searchBox.style.top = '24px';
+//     searchBox.style.pointerEvents = 'none';
+//   } else {
+//     searchBox.style.top = '72px';
+//     searchBox.style.pointerEvents = 'auto';
+//   }
+// });
+
+// menuIcon.addEventListener('click', function () {
+//   if (slideoutMenu.style.opacity == "1") {
+//     slideoutMenu.style.opacity = '0';
+//     slideoutMenu.style.pointerEvents = 'none';
+//   } else {
+//     slideoutMenu.style.opacity = '1';
+//     slideoutMenu.style.pointerEvents = 'auto';
+//   }
+// });
 
 menuIcon.addEventListener('click', function () {
-  if (slideoutMenu.style.opacity == "1") {
-    slideoutMenu.style.opacity = '0';
-    slideoutMenu.style.pointerEvents = 'none';
-  } else {
     slideoutMenu.style.opacity = '1';
     slideoutMenu.style.pointerEvents = 'auto';
-  }
-})
+
+    menuIcon.style.display = 'none';
+    menuCloseIcon.style.display = 'flex';
+});
+
+menuCloseIcon.addEventListener('click', function () {
+    slideoutMenu.style.opacity = '0';
+    slideoutMenu.style.pointerEvents = 'none';
+
+    menuIcon.style.display = 'flex';
+    menuCloseIcon.style.display = 'none';
+});
